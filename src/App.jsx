832bf61703728efd6d9c81849f4a377dcb3c0d28
Magnus Kaut <magnuskaut@gmail.com>
2024-12-24 15:03:07 +0100

    Add APOD app components and update project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,59 @@
-import TodoInput from "./components/TodoInput"
-import TodoList from "./components/TodoList"
 import { useState, useEffect } from "react"
+import Main from "./components/Main"
+import SideBar from "./components/SideBar"
+import Footer from "./components/Footer"
 
 function App() {
 	
-	const [todos, setTodos] = useState([
-		"Todo 1",
-		"Todo 2",
-		"Todo 3",
-	])
-	
-    const [todoValue, setTodoValue] = useState('')
+	const [data, setData] = useState(null)
+	const [loading, setLoading] = useState(false)
+	const [showModal, setShowModal] = useState(false)
 
-	function persistData(newList) {
-		localStorage.setItem('todos', JSON.stringify({ todos: newList }))
-	}
-
-	function handleAddTodos(NewTodo) {
-		const newTodoList = [...todos, NewTodo]
-		persistData(newTodoList)
-		setTodos(newTodoList)
-	}
-
-	function handleDeleteTodos(todoIndex) {
-		const newTodoList = todos.filter((_, index) => index !== todoIndex)
-		persistData(newTodoList)
-		setTodos(newTodoList)
-	}
-
-	function handleEditTodos(todoIndex) {
-		const valueToBeEdited = todos[todoIndex]
-		setTodoValue(valueToBeEdited)
-		handleDeleteTodos(todoIndex)
+	function handleToggleModal() {
+		setShowModal(!showModal)
 	}
 
 	useEffect(() => {
-		if (!localStorage)
-			return
-		
-		let localTodos = localStorage.getItem('todos')
-		if (!localTodos) 
-			return
-		
-		localTodos = JSON.parse(localTodos).todos
-		setTodos(localTodos)
+		const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+		async function fetchAPI() {
+			const url = "https://api.nasa.gov/planetary/apod" + 
+			`?api_key=${NASA_KEY}`
+
+			const today = new Date().toDateString()
+			const localKey = `NASA-${today}`
+
+			if (localStorage.getItem(localKey)) {
+				setData(JSON.parse(localStorage.getItem(localKey)))
+				console.log("Data fetched from local storage")
+				return
+			}
+			localStorage.removeItem(localKey)
+			try {
+				const res = await fetch(url)
+				const apiData = await res.json()
+				localStorage.setItem(localKey, JSON.stringify(apiData))
+				setData(apiData)
+				console.log("Data fetched from API")
+			} catch (error) {
+				console.error(error.message)
+			}
+		}
+		fetchAPI()
 		
 	}, [])
-
-
+	
 	return (
 		<>
-			<TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
-			<TodoList todoValue={todoValue} setTodoValue={setTodoValue} handleDeleteTodos={handleDeleteTodos} handleEditTodos={handleEditTodos} todos={todos}/>
+			{data ? (<Main data={data} />) : (
+			<div className="loadingState">
+				<i className="fa-solid fa-gear"></i>
+			</div>
+			)}
+			{showModal && 
+			(
+			<SideBar data={data} handleToggleModal={handleToggleModal}/>
+			)}
+			{data && (<Footer data={data} showModal={showModal} handleToggleModal={handleToggleModal}/>)}
 
 		</>
 	)
